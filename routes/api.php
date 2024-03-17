ae84f124bb1a583895cf18ebb9b0827e7642eb73
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-03-17 11:09:38 -0300

    criação da par autenticação e token jwt

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Routing\Route;
+
+Route::get('/', function(){
+return response()->json(['pai_name' => 'laravel-jwt', 'api_version' => '1.0.0']);
+});
+
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function(){
+    return response()->json(['pai_name' => 'laravel-jwt', 'api_version' => '1.0.0']);
+    });
+    
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    
